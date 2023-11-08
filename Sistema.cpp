f1c9f5f91e2ca2987e4c02ac8497aab6cdb74841
ValentinoVignolo <147449772+ValentinoVignolo@users.noreply.github.com>
2023-11-08 10:00:48 -0300

    dir

--- Sistema.cpp
+++ Sistema.cpp
@@ -34,7 +34,7 @@ Sistema crearNodo(Cadena nom)
 void insertarOrdenado(Sistema &s, Archivo nuevo) // insertar nodo ordenado
 {
 	Archivo aux = NULL;
-	
+
 	aux = s->Parchivo;
 	if (listaVacia(s->Parchivo))
 	{
@@ -70,7 +70,7 @@ Cadena strdup(const Cadena cade)
 	}
 	int largo = strlen(cade) + 1;
 	Cadena nuevo_cade = new char[largo];
-	
+
 	if (nuevo_cade != NULL)
 	{
 		strcpy(nuevo_cade, cade);
@@ -86,13 +86,13 @@ void insertarSistema(Sistema &s, Sistema nuevo)
 	{
 		s = nuevo;
 	}
-	else if(aux->pH == NULL)
+	else if (aux->pH == NULL)
 	{
 		aux->pH = nuevo;
 		nuevo->pPadre = aux; // actualiza el puntero al padre
 	}
 	else
-	{	
+	{
 		aux = aux->pH;
 		while (aux->sH != NULL && strcmp(aux->sH->nombre, nuevo->nombre) < 0)
 		{
@@ -138,337 +138,414 @@ TipoRet CREATEFILE(Sistema &s, Cadena nombreArchivo)
 }
 TipoRet DIR(Sistema &s, Cadena parametro)
 {
-	
+
 	Archivo aux = s->Parchivo;
-	Sistema auxsis = s,  auxher=s;
+	Sistema auxsis = s;
 	if (strcmp(parametro, "/s") == 0)
 	{
-		while(auxsis->sH)
+		if (auxsis == NULL)
 		{
-			cout<<auxsis->nombre;
-			if(aux!=NULL)	
-			{
-				cout<<"/"<<aux->nombre;
-				aux=aux->siguiente;
-			}
-			else
-			{
-				while (auxsis->sH!=NULL && auxher->sH!=NULL)
-				{
-					cout<<"/"<<auxher->sH;
-					auxher=auxher->sH;
-				}
-				
-			}	
-		}		
-	}
-	else
-	{
-		if(strcmp(parametro, "0")==0)
+			// Caso base: el sistema actual es NULL, termina la recursiÃ³n.
+			return OK;
+		}
+		Pila pil = NULL;
+		if (auxsis->Parchivo != NULL)
 		{
-			Sistema auxpadre = s;
-			Pila p = NULL;
-			while (auxpadre->pPadre != NULL)
+			Archivo auxArchivo = s->Parchivo;
+			while (auxArchivo != NULL)
 			{
-				apilar(p,crearNodoPila(auxpadre->pPadre->nombre));
-				auxpadre = auxpadre->pPadre;
-			}
-			while(p)
-			{
-				cout<<desapilar(p)->dato<<"/";
-			}
-			cout<<s->nombre<<"/"<<endl;
-			while (aux)
-			{
-				if (aux->soloLectura == 0)
+				apilar(pil, crearNodoPila(auxsis->nombre));
+				if (auxsis->pPadre != NULL)
 				{
-					cout << endl
-						<< aux->nombre << "     Lectura/Escritura";
-					aux = aux->siguiente;
+					while (strcmp(auxsis->pPadre->nombre,s->nombre)!=0  )
+					{
+						apilar(pil, crearNodoPila(auxsis->pPadre->nombre));
+						CD(auxsis, "..");
+					}
 				}
-				else if (aux->soloLectura == 1)
+				cout << endl;
+				while (pil)
 				{
-					cout << endl
-						<< aux->nombre << "     Lectura";
-					aux = aux->siguiente;
+					cout << desapilar(pil)->dato << "/";
 				}
+				cout << auxArchivo->nombre<<endl;
+				auxArchivo = auxArchivo->siguiente;
 			}
-			if(auxsis->pH != NULL)
-			{	   
-				auxsis = auxsis->pH;
-				while(auxsis !=NULL)
+
+		}
+		DIR(auxsis,"/s");
+		else if (auxsis->pH != NULL)
+		{
+			auxsis = auxsis->pH;
+			apilar(pil, crearNodoPila(auxsis->nombre));
+			DIR(auxsis,"/s");
+			mostrarP(pil);
+			if (auxsis->sH != NULL)
+			{
+				while (auxsis->sH != NULL)
 				{
-					cout << endl
-						<< auxsis->nombre;
+					apilar(pil, crearNodoPila(auxsis->sH->nombre));
+					DIR(auxsis->sH,"/s");
+					cout << endl;
+					mostrarP(pil);
 					auxsis = auxsis->sH;
 				}
-			}		
-		}
-		cout << endl;
-		return OK;
-	}
-	return NO_IMPLEMENTADA;
-}
-
-TipoRet DELETE(Sistema &s, Cadena nombreArchivo)
-{
-	Archivo aux = NULL;
-	Archivo aux2 = NULL;
-	aux = s->Parchivo;
-	while (aux->siguiente && strcmp(aux->siguiente->nombre, nombreArchivo) != 0)
-	{
-		aux = aux->siguiente;
-	}
-	if (aux->siguiente != NULL && aux->siguiente->soloLectura == (false))
-	{
-		aux2 = aux->siguiente;
-		aux->siguiente = aux2->siguiente;
-		delete aux2;
-		return OK;
-	}
-	else
-	return ERROR;
-}
-TipoRet IFF(Sistema &s, Cadena nombreArchivo, Cadena texto)
-{
-	Archivo aux = NULL;
-	aux = s->Parchivo;
-	while (aux->siguiente && strcmp(aux->siguiente->nombre, nombreArchivo) != 0)
-	{
-		aux = aux->siguiente;
-	}
-	
-	if (aux->siguiente != NULL && aux->siguiente->soloLectura == false)
-	{
-		int nuevoLargo = strlen(aux->siguiente->contenido) + strlen(texto);
-		if (nuevoLargo <= TEXTO_MAX)
-		{
-			Cadena nuevoContenido = new char[nuevoLargo + 1];
-			strcpy(nuevoContenido, texto);
-			strcat(nuevoContenido, aux->siguiente->contenido);
-			delete[] aux->siguiente->contenido;
-			aux->siguiente->contenido = nuevoContenido;
-			
-			return OK;
-		}
-		else
-		{
-			return ERROR;
-		}
-	}
-	else
-	{
-		return ERROR;
-	}
-}
-
-TipoRet TYPE(Sistema &s, Cadena nombreArchivo)
-{
-	Archivo aux = NULL;
-	aux = s->Parchivo;
-	while (aux->siguiente && strcmp(aux->siguiente->nombre, nombreArchivo) != 0)
-	{
-		aux = aux->siguiente;
-	}
-	
-	if (aux->siguiente != NULL)
-	{
-		if (strcmp(aux->siguiente->contenido, "") != 0)
-			cout << aux->siguiente->contenido << endl;
-		else
-			cout << "El archivo no tiene contenido" << endl;
-		return OK;
-	}
-	else
-	{
-		return ERROR;
-	}
-}
-TipoRet ATTRIB(Sistema &s, Cadena nombreArchivo, Cadena parametro)
-{
-	Archivo aux = NULL;
-	aux = s->Parchivo;
-	while (aux->siguiente && strcmp(aux->siguiente->nombre, nombreArchivo) != 0)
-	{
-		aux = aux->siguiente;
-	}
-	
-	if (aux->siguiente != NULL)
-	{
-		if (strcmp(parametro, "-w") == 0)
-		{
-			aux->siguiente->soloLectura = (true);
-			return OK;
-		}
-		else if (strcmp(parametro, "+w") == 0)
-		{
-			aux->siguiente->soloLectura = (false);
-			return OK;
-		}
-		else
-		{
-			cout << "Los parametros validos son +w para lectura y escritura, -w solo para lectursa" << endl;
-		}
-	}
-	else
-	{
-		return ERROR;
-	}
-	return NO_IMPLEMENTADA;
-}
-TipoRet CD(Sistema &s, Cadena nombreDirectorio)
-{
-	
-	if (strcmp(nombreDirectorio, "..") == 0)
-	{
-		// Ir al directorio padre
-		if (s->pPadre != NULL)
-		{
-			s = s->pPadre;  // Mueve al directorio padre
-			return OK;
-		}
-		else
-		{
-			cout<< "ERROR: Ya estas en el directorio raiz." << endl;
-			return ERROR;
-		}
-	}
-	else
-	{
-		// Ir al directorio hijo
-		if (s->pH != NULL)
-		{
-			Sistema aux = s->pH;
-			while (aux != NULL && strcmp(aux->nombre, nombreDirectorio) != 0)
-			{
-				aux = aux->sH;
+				
 			}
-			
-			if (aux != NULL)
+		}
+   }
+		else
+		{
+			if (strcmp(parametro, "0") == 0)
 			{
-				s = aux;
+				Sistema auxpadre = s;
+				Pila p = NULL;
+				while (auxpadre->pPadre != NULL)
+				{
+					apilar(p, crearNodoPila(auxpadre->pPadre->nombre));
+					auxpadre = auxpadre->pPadre;
+				}
+				while (p)
+				{
+					cout << desapilar(p)->dato << "/";
+				}
+				cout << s->nombre << "/" << endl;
+				while (aux)
+				{
+					if (aux->soloLectura == 0)
+					{
+						cout << endl
+							 << aux->nombre << "     Lectura/Escritura";
+						aux = aux->siguiente;
+					}
+					else if (aux->soloLectura == 1)
+					{
+						cout << endl
+							 << aux->nombre << "     Lectura";
+						aux = aux->siguiente;
+					}
+				}
+				if (auxsis->pH != NULL)
+				{
+					auxsis = auxsis->pH;
+					while (auxsis != NULL)
+					{
+						cout << endl
+							 << auxsis->nombre;
+						auxsis = auxsis->sH;
+					}
+				}
+			}
+			cout << endl;
+			return OK;
+		}
+		return NO_IMPLEMENTADA;
+	}
+
+	TipoRet DELETE(Sistema & s, Cadena nombreArchivo)
+	{
+		Archivo aux = NULL;
+		Archivo aux2 = NULL;
+		aux = s->Parchivo;
+		while (aux->siguiente && strcmp(aux->siguiente->nombre, nombreArchivo) != 0)
+		{
+			aux = aux->siguiente;
+		}
+		if (aux->siguiente != NULL && aux->siguiente->soloLectura == (false))
+		{
+			aux2 = aux->siguiente;
+			aux->siguiente = aux2->siguiente;
+			delete aux2;
+			return OK;
+		}
+		else
+			return ERROR;
+	}
+	TipoRet IFF(Sistema & s, Cadena nombreArchivo, Cadena texto)
+	{
+		Archivo aux = NULL;
+		aux = s->Parchivo;
+		while (aux->siguiente && strcmp(aux->siguiente->nombre, nombreArchivo) != 0)
+		{
+			aux = aux->siguiente;
+		}
+
+		if (aux->siguiente != NULL && aux->siguiente->soloLectura == false)
+		{
+			int nuevoLargo = strlen(aux->siguiente->contenido) + strlen(texto);
+			if (nuevoLargo <= TEXTO_MAX)
+			{
+				Cadena nuevoContenido = new char[nuevoLargo + 1];
+				strcpy(nuevoContenido, texto);
+				strcat(nuevoContenido, aux->siguiente->contenido);
+				delete[] aux->siguiente->contenido;
+				aux->siguiente->contenido = nuevoContenido;
+
 				return OK;
 			}
 			else
 			{
-				cout << "ERROR: No existe el subdirectorio destino." << endl;
 				return ERROR;
 			}
 		}
 		else
 		{
-			cout << "Error: No hay subdirectorios en el directorio actual." << endl;
+			return ERROR;
 		}
 	}
-	
-	
-	return NO_IMPLEMENTADA;
-}
-TipoRet MKDIR (Sistema &s, Cadena nombreDirectorio)
-{
-	Sistema aux = s;
-	Cadena CF = new char;
-	strcpy(CF, "Raiz");
-	if (nombreDirectorio != CF)
-	{ 
-		if(aux->pH !=NULL)
-		{	
-			aux = aux->pH;
-			while (aux != NULL)
-			{
-				if (strcmp(aux->nombre, nombreDirectorio) == 0)
-				{
-					return ERROR;
-				}
-				aux = aux->sH;
-			}
-			insertarSistema(s, crearNodo(nombreDirectorio));
+
+	TipoRet TYPE(Sistema & s, Cadena nombreArchivo)
+	{
+		Archivo aux = NULL;
+		aux = s->Parchivo;
+		while (aux->siguiente && strcmp(aux->siguiente->nombre, nombreArchivo) != 0)
+		{
+			aux = aux->siguiente;
+		}
+
+		if (aux->siguiente != NULL)
+		{
+			if (strcmp(aux->siguiente->contenido, "") != 0)
+				cout << aux->siguiente->contenido << endl;
+			else
+				cout << "El archivo no tiene contenido" << endl;
 			return OK;
 		}
 		else
 		{
-			insertarSistema(s, crearNodo(nombreDirectorio));
+			return ERROR;
+		}
+	}
+	TipoRet ATTRIB(Sistema & s, Cadena nombreArchivo, Cadena parametro)
+	{
+		Archivo aux = NULL;
+		aux = s->Parchivo;
+		while (aux->siguiente && strcmp(aux->siguiente->nombre, nombreArchivo) != 0)
+		{
+			aux = aux->siguiente;
+		}
+
+		if (aux->siguiente != NULL)
+		{
+			if (strcmp(parametro, "-w") == 0)
+			{
+				aux->siguiente->soloLectura = (true);
+				return OK;
+			}
+			else if (strcmp(parametro, "+w") == 0)
+			{
+				aux->siguiente->soloLectura = (false);
+				return OK;
+			}
+			else
+			{
+				cout << "Los parametros validos son +w para lectura y escritura, -w solo para lectursa" << endl;
+			}
+		}
+		else
+		{
+			return ERROR;
+		}
+		return NO_IMPLEMENTADA;
+	}
+	TipoRet CD(Sistema & s, Cadena nombreDirectorio)
+	{
+
+		if (strcmp(nombreDirectorio, "..") == 0)
+		{
+			// Ir al directorio padre
+			if (s->pPadre != NULL)
+			{
+				s = s->pPadre; // Mueve al directorio padre
+				return OK;
+			}
+			else
+			{
+				cout << "ERROR: Ya estas en el directorio raiz." << endl;
+				return ERROR;
+			}
+		}
+		else
+		{
+			// Ir al directorio hijo
+			if (s->pH != NULL)
+			{
+				Sistema aux = s->pH;
+				while (aux != NULL && strcmp(aux->nombre, nombreDirectorio) != 0)
+				{
+					aux = aux->sH;
+				}
+
+				if (aux != NULL)
+				{
+					s = aux;
+					return OK;
+				}
+				else
+				{
+					cout << "ERROR: No existe el subdirectorio destino." << endl;
+					return ERROR;
+				}
+			}
+			else
+			{
+				cout << "Error: No hay subdirectorios en el directorio actual." << endl;
+			}
+		}
+
+		return NO_IMPLEMENTADA;
+	}
+	TipoRet MKDIR(Sistema & s, Cadena nombreDirectorio)
+	{
+		Sistema aux = s;
+		Cadena CF = new char;
+		strcpy(CF, "Raiz");
+		if (nombreDirectorio != CF)
+		{
+			if (aux->pH != NULL)
+			{
+				aux = aux->pH;
+				while (aux != NULL)
+				{
+					if (strcmp(aux->nombre, nombreDirectorio) == 0)
+					{
+						return ERROR;
+					}
+					aux = aux->sH;
+				}
+				insertarSistema(s, crearNodo(nombreDirectorio));
+				return OK;
+			}
+			else
+			{
+				insertarSistema(s, crearNodo(nombreDirectorio));
+				return OK;
+			}
+		}
+		else
+		{
+			return ERROR;
+		}
+		return NO_IMPLEMENTADA;
+	}
+	TipoRet RMDIR(Sistema & s, Cadena nombreDirectorio)
+	{
+		Sistema auxActual = s;
+		Sistema auxSub = s->pH;
+		// Si el directorio es el primer hijo
+		if (strcmp(auxActual->pH->nombre, nombreDirectorio) == 0)
+		{
+			s->pH = auxActual->pH->sH;
+			DESTRUIRSISTEMA(auxSub);
 			return OK;
 		}
-	}
-	else
-	{
-		return ERROR;
-	}
-	return NO_IMPLEMENTADA;
-}
-TipoRet RMDIR (Sistema &s, Cadena nombreDirectorio)
-{	
-	Sistema auxActual = s;
-	Sistema auxSub = s->pH;
-	//Si el directorio es el primer hijo
-	if(strcmp (auxActual->pH->nombre,nombreDirectorio)==0)
-	{
-		s->pH = auxActual->pH->sH;
-		DESTRUIRSISTEMA(auxSub);
-		return OK;
-		
-	}
-	// Busca el directorio a eliminar
-	while (auxSub->sH != NULL && strcmp(auxSub->sH->nombre, nombreDirectorio) != 0) 
-	{
-		auxSub = auxSub->sH;
-	}
-	// Elimina todos los archivos y subdirectorios del directorio a eliminar
-	if (auxSub->sH != NULL) 
-	{
-		Sistema AuxElim = auxSub->sH;
-		auxSub->sH = AuxElim->sH;
-		DESTRUIRSISTEMA(AuxElim);
-		cout<<"DEBUG: destruir sistema devolvio"<<endl;
-		return OK;
-		
-	}
-	else
-	{
-	// No se encontró el directorio a eliminar
-	return ERROR;
-	}
-	return NO_IMPLEMENTADA;
-}
-TipoRet MOVE (Sistema &s, Cadena nombre, Cadena directorioDestino)
-{
-	return NO_IMPLEMENTADA;
-}
-TipoRet DESTRUIRSISTEMA(Sistema &s)
-{
-	if (s != NULL) 
-	{
-		// Llama a la funcion para destruir la lista de archivos apuntada por este sistema
-		destruirListaArchivos(s->Parchivo);
-		
-		// Llama a la funcion para destruir recursivamente los siguientes hijos de este sistema
-		if(s->pH != NULL)
+		// Busca el directorio a eliminar
+		while (auxSub->sH != NULL && strcmp(auxSub->sH->nombre, nombreDirectorio) != 0)
 		{
-			if(s->pH->sH != NULL)
-				DESTRUIRSISTEMA(s->pH->sH);
+			auxSub = auxSub->sH;
 		}
-		
-		// Llama a la funcion para destruir recursivamente el primer hijos de este sistema
-		if(s->pH != NULL)
-		   DESTRUIRSISTEMA(s->pH);
-		
-		// Libera la memoria del nodo actual
-		delete s;
+		// Elimina todos los archivos y subdirectorios del directorio a eliminar
+		if (auxSub->sH != NULL)
+		{
+			Sistema AuxElim = auxSub->sH;
+			auxSub->sH = AuxElim->sH;
+			DESTRUIRSISTEMA(AuxElim);
+			cout << "DEBUG: destruir sistema devolvio" << endl;
+			return OK;
+		}
+		else
+		{
+			// No se encontrï¿½ el directorio a eliminar
+			return ERROR;
+		}
+		return NO_IMPLEMENTADA;
 	}
-	// Asigna NULL al puntero del sistema para decirle que ya no hay estructuras validas
-	s = NULL;
-	return OK;	
-
-}
-void destruirListaArchivos(Archivo &archi) 
-{
-	while (archi != NULL) 
+	TipoRet MOVE(Sistema & s, Cadena nombre, Cadena directorioDestino)
 	{
-		Archivo auxElim = archi;
-		archi = archi->siguiente;
-		delete auxElim->nombre;
-		delete auxElim->contenido;
-		delete auxElim;
+		//		Sistema auxcpy = NULL, auxsis = s;
+		//		Archivo auxArchivo = s->Parchivo;
+		//		if (auxsis->pH != NULL)
+		//		{
+		//			if (strcmp(auxsis->pH->nombre, nombre) == 0)
+		//			{
+		//				auxsis = auxsis->pH;
+		//				s->pH = auxsis->pH->sH;
+		//
+		//				return OK;
+		//			}
+		//			auxsis = auxsis->pH;
+		//			// Busca el directorio a Mover
+		//			while (auxsis->sH != NULL && strcmp(auxsis->sH->nombre, nombreDirectorio) != 0)
+		//			{
+		//				auxsis = auxsis->sH;
+		//			}
+		//
+		//			if (auxSub->sH != NULL)
+		//			{
+		//				Sistema AuxElim = auxSub->sH;
+		//				auxSub->sH = AuxElim->sH;
+		//
+		//				return OK;
+		//			}
+		//			else
+		//			{
+		//				return ERROR;
+		//			}
+		//		}
+		//		else
+		//		{
+		//			cout << "No hay subdirectorios en este directorio" << endl;
+		//		}
+		return NO_IMPLEMENTADA;
 	}
-}
+	TipoRet DESTRUIRSISTEMA(Sistema & s)
+	{
+		if (s != NULL)
+		{
+			// Llama a la funcion para destruir la lista de archivos apuntada por este sistema
+			destruirListaArchivos(s->Parchivo);
+
+			// Llama a la funcion para destruir recursivamente los siguientes hijos de este sistema
+			if (s->pH != NULL)
+			{
+				if (s->pH->sH != NULL)
+					DESTRUIRSISTEMA(s->pH->sH);
+			}
+
+			// Llama a la funcion para destruir recursivamente el primer hijos de este sistema
+			if (s->pH != NULL)
+				DESTRUIRSISTEMA(s->pH);
+
+			// Libera la memoria del nodo actual
+			delete s;
+		}
+		// Asigna NULL al puntero del sistema para decirle que ya no hay estructuras validas
+		s = NULL;
+		return OK;
+	}
+	void destruirListaArchivos(Archivo & archi)
+	{
+		while (archi != NULL)
+		{
+			Archivo auxElim = archi;
+			archi = archi->siguiente;
+			delete auxElim->nombre;
+			delete auxElim->contenido;
+			delete auxElim;
+		}
+	}
+  Pila DirRecursivo(Sistema s,Pila& P)
+  {
+     Sistema aux=s, auxher=s;
+     if(aux==NULL)
+     {
+        return P;
+     }
+     else
+     {
+        if()
+        apilar(p, crearNodoPila(aux->pH->nombre);
+        aux=aux->pH;
+        DirRecursivo(aux,P)
+     }
+  }
